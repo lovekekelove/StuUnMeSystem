567009a9e66d0b8a3af481346458d4ae3c211512
Administrator <1371984404@qq.com>
2018-01-08 09:32:49 +0800

    js

---
+++ src/main/webapp/js/index.js
@@ -0,0 +1,13 @@
+$(function () {
+
+    $(".J_menuItem").on('click', function () {
+        var url = $(this).attr('href');
+        $("#J_iframe").attr('src', url);
+        return false;
+    });
+
+    $('#searchform').submit(function (e) {
+        $('#J_iframe').attr('src', '/show/search?searchkey=' + $('#searchkey').val());
+        return false;
+    });
+});
