456db40403e46ed5636058e53b9b254eb28a272b
Administrator <1371984404@qq.com>
2018-01-26 19:26:56 +0800

    留言

--- src/main/java/com/stumesystem/controller/LeaveMsgController.java
+++ src/main/java/com/stumesystem/controller/LeaveMsgController.java
@@ -15,7 +15,7 @@ import java.util.Date;
 import java.util.List;
 
 @Controller
-public class leaveMsgController {
+public class LeaveMsgController {
 
     @Autowired
     private LeaveMsgService leaveMsgService;
